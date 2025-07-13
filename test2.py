9485e7921915c7e8c9cdcb35f5ea9d2f45936f32
Vladislav-Kishino <vladislavkishino@gmail.com>
2025-07-13 18:38:43 +0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,11 @@
 #Задание №2
 # Запрашиваем этапы развития человека
-print("Введите этапы развития человека по порядку (последний - Homo sapiens sapiens):")
+print("Введите этапы развития человека по порядку:")
 stage1 = input("Первый этап: ")
 stage2 = input("Второй этап: ")
 stage3 = input("Третий этап: ")
 stage4 = input("Четвертый этап: ")
-stage5 = input("Пятый этап (Homo sapiens sapiens): ")
+stage5 = input("Пятый этап: ")
 
 # Выводим этапы с разделителем =>
 print(stage1, stage2, stage3, stage4, stage5, sep=' => ')
